a05c92204cb7a315e7b393892899c0b0d2d5f1af
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-03-10 19:20:54 +0600

    Modify existing component, add new component and style new component

---
+++ src/components/Course.jsx
@@ -0,0 +1,14 @@
+import "./Course.css";
+
+const Course = () => {
+  return (
+    <div className="Course">
+        <h2 className="CourseHeading">Course</h2>
+        <h3> * October 2018 - October 2018 *</h3>
+        <h4>Certificate of Attendance on Mastercam NC Programming</h4>
+        <h5>at IME CADCAM Training Centre, Penang, Malaysia</h5>
+    </div>
+  );
+}
+
+export default Course;
