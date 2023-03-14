a98a3af345c564ad6533cc35c27c5edce567842f
Md Jahid Hasan <80060849+hmjahid@users.noreply.github.com>
2023-03-14 22:33:29 +0600

    Add box-shadow to headings

--- src/components/Greetings.jsx
+++ src/components/Greetings.jsx
@@ -2,7 +2,7 @@ import Invitation from "./Invitation";
 
 function Starter(){
     return (
-    <p style={{color: "red", margin: "0px 28px 0px 28px", backgroundColor: "white", padding:"5px", fontWeight:"bold", borderRadius: "8px", fontSize: "24px"}}>*** Starting By The Name Of Almighty ALLAH ***</p>
+    <p style={{color: "red", margin: "0px 28px 0px 28px", backgroundColor: "white", padding:"5px", fontWeight:"bold", borderRadius: "8px", fontSize: "24px", boxShadow: "5px 5px 5px 5px black"}}>*** Starting By The Name Of Almighty ALLAH ***</p>
     );
 }
 const Greetings = function (){
